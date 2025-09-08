57acd73621a47f5e3adb4f41b2db9c541641bad1
Daniel Butterfield <danielcbutter@gmail.com>
2025-09-08 16:14:30 +0000

    Remove debug statements from install.sh now that it works

--- install.sh
+++ install.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Make script exit on first failure
+# Make script fail if anything goes wrong
 set -euo pipefail
-set -x   # show each command and its expanded args
 
 # Navigate into the directory
 ROMAN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROMAN_DIR
-echo "HELLO"
-echo $ROMAN_DIR
-
 # Install gdown if not already installed
 pip install gdown
 
-echo "WHAT"
-echo $CI_TESTS
 # For GitHub Actions, set directory as safe so submodule update works
 if [ "$CI_TESTS" = "true" ]; then
   git config --global --add safe.directory "$ROMAN_DIR"
-  echo "ACTION RAN"
 fi
 
 # Install CLIPPER
